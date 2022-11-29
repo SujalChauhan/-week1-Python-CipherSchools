2c25764d8703d8d7e5bdaa84cc5c2719b114f2ce
SujalChauhan <115447523+SujalChauhan@users.noreply.github.com>
2022-11-29 16:51:55 +0530

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,3 @@
+print(' I\'m Sujal Chauhan ')
+print('Be it \\ dfg')
+print("git bash \"")
---
+++ class3.py
@@ -0,0 +1,3 @@
+print(' I\'m Sujal Chauhan ') # \' is for '
+print('Be it \\ dfg') # \\ is for \
+print("git bash \"") # \" to print "
